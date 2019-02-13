606f2356048c1d12eb9aaacc997cb8566372a3af
Stuart Scott <stuart.murray.scott@gmail.com>
2019-02-13 13:39:10 -0800

    Handle Alias GET/POST

--- bcnet.go
+++ bcnet.go
@@ -21,8 +21,10 @@ import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
+	"github.com/AletheiaWareLLC/aliasgo"
 	"github.com/AletheiaWareLLC/bcgo"
 	"github.com/golang/protobuf/proto"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
@@ -74,6 +76,134 @@ func GetSecurityStore() (string, error) {
 
 type KeyStore struct {
 	Keys map[string]*bcgo.KeyShare
+}
+
+func HandleAlias(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.RemoteAddr, r.Proto, r.Method, r.Host, r.URL.Path)
+	aliases, err := aliasgo.OpenAliasChannel()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	switch r.Method {
+	case "GET":
+		query := r.URL.Query()
+		var a string
+		if results, ok := query["alias"]; ok && len(results) == 1 {
+			a = results[0]
+		}
+		log.Println("Alias", a)
+		if err := aliasgo.UniqueAlias(aliases, a); err != nil {
+			log.Println(err)
+			// TODO warn user
+			return
+		}
+		var publicKey string
+		if results, ok := query["publicKey"]; ok && len(results) == 1 {
+			publicKey = results[0]
+		}
+		log.Println("PublicKey", publicKey)
+		t, err := template.ParseFiles("html/template/alias.html")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		data := struct {
+			Alias     string
+			PublicKey string
+		}{
+			Alias:     a,
+			PublicKey: publicKey,
+		}
+		log.Println("Data", data)
+		err = t.Execute(w, data)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	case "POST":
+		r.ParseForm()
+		log.Println("Request", r)
+		a := r.Form["alias"]
+		log.Println("Alias", a)
+		publicKey := r.Form["publicKey"]
+		log.Println("PublicKey", publicKey)
+		publicKeyFormat := r.Form["publicKeyFormat"]
+		log.Println("PublicKeyFormat", publicKeyFormat)
+		signature := r.Form["signature"]
+		log.Println("Signature", signature)
+		signatureAlgorithm := r.Form["signatureAlgorithm"]
+		log.Println("SignatureAlgorithm", signatureAlgorithm)
+
+		if err := aliasgo.UniqueAlias(aliases, a[0]); err != nil {
+			log.Println(err)
+			return
+		}
+
+		pubKey, err := base64.RawURLEncoding.DecodeString(publicKey[0])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		pubFormatValue, ok := bcgo.PublicKeyFormat_value[publicKeyFormat[0]]
+		if !ok {
+			log.Println("Unrecognized Public Key Format")
+			return
+		}
+		pubFormat := bcgo.PublicKeyFormat(pubFormatValue)
+
+		sig, err := base64.RawURLEncoding.DecodeString(signature[0])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		sigAlgValue, ok := bcgo.SignatureAlgorithm_value[signatureAlgorithm[0]]
+		if !ok {
+			log.Println("Unrecognized Signature")
+			return
+		}
+		sigAlg := bcgo.SignatureAlgorithm(sigAlgValue)
+
+		record, err := aliasgo.CreateAliasRecord(a[0], pubKey, pubFormat, sig, sigAlg)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		data, err := proto.Marshal(record)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		entries := [1]*bcgo.BlockEntry{
+			&bcgo.BlockEntry{
+				RecordHash: bcgo.Hash(data),
+				Record:     record,
+			},
+		}
+
+		node, err := bcgo.GetNode()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// Mine record into blockchain
+		hash, block, err := node.MineRecords(aliases, entries[:])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err := aliases.Multicast(hash, block); err != nil {
+			log.Println(err)
+			return
+		}
+	default:
+		log.Println("Unsupported method", r.Method)
+	}
 }
 
 func (ks *KeyStore) HandleKeys(w http.ResponseWriter, r *http.Request) {
@@ -296,7 +426,7 @@ func HandleUpdate(conn net.Conn) {
 		log.Println(err)
 		// return - Must send head reference back
 	}
-	reference := &Reference{
+	reference := &bcgo.Reference{
 		Timestamp:   c.HeadBlock.Timestamp,
 		ChannelName: c.Name,
 		BlockHash:   c.HeadHash,
